f549bda2d6d6bfa67094bdf93440ea136d0b7145
= <Luis.PARODY@naranja.com>
2019-09-14 10:51:28 -0300

    Variable entorno heroku MONGODB_URI

--- server/config/config.js
+++ server/config/config.js
@@ -11,12 +11,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // base de datos
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//    urlDB =  'mongodb://localhost:27017/cafe';
-//} else {
-//    urlDB =  'mongodb+srv://admin:3uKu6fJnt2m1yjlg@cluster0-lh7um.mongodb.net/test?retryWrites=true&w=majority':
-//}
-urlDB =  'mongodb+srv://admin:3uKu6fJnt2m1yjlg@cluster0-lh7um.mongodb.net/test?retryWrites=true&w=majority';
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB =  'mongodb://localhost:27017/cafe';
+} else {
+    urlDB =  process.env.MONGODB_URI;
+}
+//urlDB =  'mongodb+srv://admin:3uKu6fJnt2m1yjlg@cluster0-lh7um.mongodb.net/test?retryWrites=true&w=majority';
 
 process.env.URLDB = urlDB;
 
